f616abb5d72b34fcf4b67fd667d48f123d72a157
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-10-21 17:52:18 +0200

    Add lab03 docker

---
+++ .devcontainer/lab03/arm_03_run_gpu_nvidia.sh
@@ -0,0 +1,33 @@
+xhost +local:root
+
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+docker stop ARM_03 || true && docker rm ARM_03 || true
+
+docker run -it \
+    --gpus all \
+    --workdir="/arm_ws" \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume=/home/gwidon/Documents/ARM/src:/arm_ws/src/port \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --env="NVIDIA_VISIBLE_DEVICES=all" \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --privileged \
+    --network=host \
+    --name="ARM_03" \
+    arm/lab03 \
+    /bin/bash
