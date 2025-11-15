638d0f01f1fb07859622148526ad60ef54430d3e
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-11-15 22:26:20 +0100

    Copy BT from previous lab

---
+++ src/arm05/turtlebot3_bt/bt_scripts/run_bt.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+import py_trees
+import py_trees_ros.trees
+import py_trees.console as console
+import rclpy
+import sys
+import behaviours
+from py_trees_ros import subscribers as py_trees_subscribers
+import sensor_msgs.msg as sensor_msgs
+
+def create_root() -> py_trees.behaviour.Behaviour:
+    # Kompozycja typu Parallel, będzie korzeniem naszego drzewa. 
+    # Wszyskie połączone do niego dzieci będą wykonywane równocześnie
+    root = py_trees.composites.Parallel( 
+        name="Turtlebot3 Behaviour Tree",
+        policy=py_trees.common.ParallelPolicy.SuccessOnAll(
+            synchronise=True
+        )
+    )
+
+    # zachowanie Rotate, powoduje obrót robota w miejscu.
+    rotation = behaviours.Rotate(name="RotateRobot") 
+
+    # Utworzenie zachowania Drive (należy zaimplementować), które wykonuje ruch do przodu.
+    driving = behaviours.Drive(name="MoveForward")
+    
+    # zachowanie subskrybujące skan lasera z robota i przekazujące 
+    # go do Tablicy (przekazuje wyłącznie pole "ranges")
+    laser2BB = py_trees_subscribers.ToBlackboard(name="Laser2BB",
+                                                 topic_name="/scan",
+                                                 topic_type=sensor_msgs.LaserScan,
+                                                 blackboard_variables={'/scan': 'ranges'},
+                                                 qos_profile=py_trees_ros.utilities.qos_profile_unlatched(), 
+                                                 clearing_policy=py_trees.common.ClearingPolicy.NEVER)
+    
+    # funkcja sprawdzająca czy odległość do przeszkody jest większa niż 3 metry
+    # musi odczytać wartości skanera zapisane w Tablicy 
+    # (tablica jest domyślnie przekazywana przez EternalGuard)
+    def check_distance_in_front(blackboard: py_trees.blackboard.Blackboard) -> bool:
+        DISTANCE_THRESHOLD = 3
+        scan = blackboard.scan
+        # print(scan)
+        scan_length = len(scan)
+        range_per_index = 360 / scan_length
+        indexes_for_10_degrees = int(10 / range_per_index)  
+
+        front_ranges = []
+
+        for i in range(-indexes_for_10_degrees, indexes_for_10_degrees + 1):
+            index = i % scan_length  # obsługa cykliczności dla ujemnych indeksów
+            front_ranges.append(scan[index])
+
+        # Sprawdzanie czy dystans jest większy niż 3 metry.
+        if min(front_ranges) > DISTANCE_THRESHOLD:
+            return True
+        return False
+    
+    # Dodanie dekoratora EternalGuard z warunkiem dystansu do przeszkody do jazdy na wprost.
+    distance_check = py_trees.decorators.EternalGuard("Distance Check", driving, check_distance_in_front, blackboard_keys=['scan']) 
+    
+    # TODO: Modyfikacja drzewa tak, żeby było możliwe realizowanie założeń z instrukcji.
+    root.add_child(laser2BB)
+    root.add_child(rotation)
+    root.add_child(distance_check)
+    
+    # Zwracanie korzenia i połączonych do niego dzieci
+    return root
+
+def main():
+    rclpy.init()
+    root = create_root()
+
+    # tworzenie drzewa z korzenia (istotne jest użycie klasy z rozszerzenia "_ros",
+    # aby drzewo było nodem)
+    tree = py_trees_ros.trees.BehaviourTree(
+        root=root,
+        unicode_tree_debug=True
+    )
+    try:
+        tree.setup(timeout=15)
+    except py_trees_ros.exceptions.TimedOutError as e:
+        console.logerror(console.red + "failed to setup the tree, aborting [{}]".format(str(e)) + console.reset)
+        tree.shutdown()
+        rclpy.try_shutdown()
+        sys.exit(1)
+    except KeyboardInterrupt:
+        console.logerror("tree setup interrupted")
+        tree.shutdown()
+        rclpy.try_shutdown()
+        sys.exit(1)
+
+    # metoda tick_tock powoduje cykliczne uruchamianie drzewa z przerwą między wywołaniami.
+    # możliwe jest przekazanie parametru "number_of_iterations" aby określić ile razy ma być wywołane
+    # dostępna jest również metoda tick, wykonująca pojedyncze wywołanie drzewa
+    tree.tick_tock(period_ms=1000.0)
+
+    try:
+        rclpy.spin(tree.node)
+    except (KeyboardInterrupt, rclpy.executors.ExternalShutdownException):
+        pass
+    finally:
+        tree.shutdown()
+        rclpy.try_shutdown()
+
+if __name__ == '__main__':
+    main()
